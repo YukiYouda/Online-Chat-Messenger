d173a392f930c59d19604a9268065a918e6cbea8
youda <youda0518@yahoo.co.jp>
2024-07-04 17:19:53 +0900

    実装完了

--- app/stage1/client.py
+++ app/stage1/client.py
@@ -1,4 +1,15 @@
 import socket
+import time
+import threading
+
+def recieve_response(sock):
+    while True:
+        try:
+            data, server = sock.recvfrom(4906)
+            print('\nreceives {!r}'.format(data))
+        except Exception as e:
+            print(f'\n予期しないエラーが発生しました: {e}')
+            break
 
 def protocol_header(username_length):
     return username_length.to_bytes(1, "big")
@@ -10,36 +21,60 @@ server_address = '0.0.0.0'
 server_port = 9001
 
 address = ''
-port = 9050
+port = 9051
 
 user_name = input('ユーザー名を入力してください : ')
 user_name_bits = user_name.encode('utf-8')
 
 sock.bind((address, port))
 
-message = input('メッセージを入力してください : ')
-message_bits = message.encode('utf-8')
+# タイムアウトの設定
+timeout = 100
+last_send_time = time.time()
 
-try:
-    print('sending {!r}'.format(message))
-    # ヘッダー情報の作成
-    header = protocol_header(len(user_name_bits))
-    print(header)
+# 応答受信用スレッドの開始
+thread = threading.Thread(target=recieve_response, args=(sock,))
+thread.daemon = True
+thread.start()
 
-    # ヘッダーの送信
-    sock.sendto(header, (server_address, server_port))
+while True:
+    message = input('メッセージを入力してください : ')
+    message_bits = message.encode('utf-8')
+    message_bits_length = len(message_bits)
 
-    # サーバーへユーザーネームの送信
-    sock.sendto(user_name_bits, (server_address, server_port))
+    # 4096バイト以上のメッセージは送信できないようにする
+    if message_bits_length > 4096:
+        print('メッセージの最大は4096バイトです')
+        continue
 
-    # サーバーメッセージの送信
-    sock.sendto(message_bits, (server_address, server_port))
+    # messageの送信時間
+    current_time = time.time()
 
-    # 応答を受信
-    print('waiting to receive')
-    data, server = sock.recvfrom(4906)
-    print('receives {!r}'.format(data))
+    # 一定時間経過したかどうかチェック
+    if current_time - last_send_time >= timeout:
+        print('タイムアウトです')
+        sock.close()
+        break
 
-finally:
-    print('closing socket')
-    sock.close()
+    try:
+        print('sending {!r}'.format(message))
+        # ヘッダー情報の作成
+        header = protocol_header(len(user_name_bits))
+        print(header)
+
+        # サーバーへヘッダーの送信
+        sock.sendto(header, (server_address, server_port))
+
+        # サーバーへユーザーネームの送信
+        sock.sendto(user_name_bits, (server_address, server_port))
+
+        # サーバーへメッセージの送信
+        sock.sendto(message_bits, (server_address, server_port))
+
+        # 最後の送信時間を更新
+        last_send_time = current_time
+
+    except Exception as e:
+        print(f"予期しないエラーが発生しました: {e}")
+        print('closing socket')
+        sock.close()
--- app/stage1/server.py
+++ app/stage1/server.py
@@ -1,6 +1,6 @@
 import socket
-import os
-from pathlib import Path
+import select
+import time
 
 # AF_INETを使用し、UDPソケットを作成
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -13,18 +13,47 @@ print('Starting up on {} port {}'.format(server_address, server_port))
 # ソケットをアドレスとポートに紐付け
 sock.bind((server_address, server_port))
 
+# 接続しているユーザー情報
+user_name = ''
+user_info = {}
+last_message_time = {}
+
+# タイムアウトの設定
+timeout = 100
+
 while True:
     print('\nwaiting to receive message')
-    header, address = sock.recvfrom(1)
-    username_length = int.from_bytes(header[:1], "big")
-    user_name = sock.recvfrom(username_length)[0].decode('utf-8')
 
-    # ユーザー情報の登録
-    user_info = {}
-    user_info[user_name] = address
+    # selectを使って読み取り可能なソケットを監視
+    ready = select.select([sock], [], [], timeout)
 
-    # メッセージの受信
-    message = sock.recvfrom(4096)
+    if ready[0]:
+        header, address = sock.recvfrom(1)
+        username_length = int.from_bytes(header[:1], "big")
+        user_name = sock.recvfrom(username_length)[0].decode('utf-8')
 
-    if message:
-        sock.sendto(*message)
+        # ユーザー情報の登録
+        user_info[user_name] = address
+
+        # メッセージの受信
+        message = sock.recvfrom(4096)[0]
+
+        # 現在の時刻を取得
+        current_receive_time = time.time()
+
+        # 最終メッセージ受信時刻を更新
+        last_message_time[address] = current_receive_time
+
+        if message:
+            for value in user_info.values():
+                sock.sendto(message, value)
+                print('sending {} {}'.format(message, value))
+        print(user_info)
+    else:
+        if user_name in user_info:
+            current_time = time.time()
+            for client_address, last_time in list(last_message_time.items()):
+                if current_time - last_time >= timeout:
+                    print(f'No message received from {client_address} within the timeout period. Removing client.')
+                    del last_message_time[client_address]
+                    del user_info[user_name]
