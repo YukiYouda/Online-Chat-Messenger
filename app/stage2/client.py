c40cc5a00ae299bf2b91d69b5bf5e6b9468fd63f
youda <youda0518@yahoo.co.jp>
2024-07-10 15:34:21 +0900

    実装完了

--- app/stage2/client.py
+++ app/stage2/client.py
@@ -1,34 +1,186 @@
 import socket
 import sys
+import math
+import json
+import base64
+import threading
+import time
 
-def protocol_header(room_name_size, operation, state, operation_payload_size):
+# TCP通信のヘッダー作成用の関数
+def protocol_tcp_header(room_name_size, operation, state, operation_payload_size):
     return room_name_size.to_bytes(1, "big") + operation.to_bytes(1, "big") + state.to_bytes(1, "big") + operation_payload_size.to_bytes(29, "big")
 
+# UDP通信のヘッダー作成用の関数
+def protocol_udp_header(room_name_size, token_size):
+    return room_name_size.to_bytes(1, "big") + token_size.to_bytes(1, "big")
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# サーバーからの返信受け取り用の関数
+def recieve_response(sock):
+    while True:
+        try:
+            data, server = sock.recvfrom(4096)
+            print('\nreceives {!r}'.format(data))
+        except Exception as e:
+            print(f'\n予期しないエラーが発生しました: {e}')
+            break
+
+sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_address = '0.0.0.0'
-server_port = 9001
+tcp_server_port = 9001
+udp_sever_port = 9002
 
-print('connecting to {}'.format(server_address, server_port))
+print('connecting to {}'.format(server_address, tcp_server_port))
 
 try:
-    sock.connect((server_address, server_port))
+    sock_tcp.connect((server_address, tcp_server_port))
 except socket.error as err:
     print(err)
     sys.exit(1)
 
-try:
-    room_name = 'room_name'
-    room_name_bits = room_name.encode('utf-8')
-    operation = 1
-    state = 1
-    operation_payload = 'operation'
-    operation_payload_bits = operation_payload.encode('utf-8')
-    header = protocol_header(len(room_name_bits), operation, state, len(operation_payload_bits))
+token = ''
+room_name = ''
 
-    sock.send(header)
+try:
+    while True:
+        operation = int(input('操作コードを入力してください : '))
+
+        # 操作コードが0のとき
+        if operation == 0:
+            user_name = input('ユーザー名を入力してください : ')
+            room_name = ''
+            room_name_bits = room_name.encode('utf-8')
+            state = 0
+            operation_payload = user_name
+            operation_payload_bits = operation_payload.encode('utf-8')
+            header = protocol_tcp_header(len(room_name_bits), operation, state, len(operation_payload_bits))
+
+            # サーバーにヘッダーの送信
+            sock_tcp.send(header)
+
+            # サーバーにルーム名の送信
+            sock_tcp.send(room_name_bits)
+
+            # サーバーにopreration_payloadの送信
+            sock_tcp.send(operation_payload_bits)
+
+        # 操作コードが1のとき(ルームの新規作成)
+        if operation == 1:
+            room_name = input('作成するルーム名を入力してください : ')
+            room_name_bits = room_name.encode('utf-8')
+            state = 0
+            operation_payload = ''
+            operation_payload_bits = operation_payload.encode('utf-8')
+            header = protocol_tcp_header(len(room_name_bits), operation, state, len(operation_payload_bits))
+
+            # サーバーにヘッダーの送信
+            sock_tcp.send(header)
+
+            # サーバーにルーム名の送信
+            sock_tcp.send(room_name_bits)
+
+            # サーバーにopreration_payloadの送信
+            sock_tcp.send(operation_payload_bits)
+
+            # サーバーからの応答を受信
+            data = json.loads(sock_tcp.recv(int(math.pow(2, 29))).decode('utf-8'))
+            print(data)
+            token = data['200']
+
+            # tcp接続を閉じる
+            sock_tcp.close()
+            break
+
+        # 操作コードが2のとき(既存のルームに参加)
+        if operation == 2:
+            room_name = input('参加するルーム名を入力してください : ')
+            room_name_bits = room_name.encode('utf-8')
+            state = 0
+            operation_payload = ''
+            operation_payload_bits = operation_payload.encode('utf-8')
+            header = protocol_tcp_header(len(room_name_bits), operation, state, len(operation_payload_bits))
+
+            # サーバーにヘッダーの送信
+            sock_tcp.send(header)
+
+            # サーバーにルーム名の送信
+            sock_tcp.send(room_name_bits)
+
+            # サーバーにopreration_payloadの送信
+            sock_tcp.send(operation_payload_bits)
+
+            # サーバーからの応答を受信
+            data = json.loads(sock_tcp.recv(int(math.pow(2, 29))).decode('utf-8'))
+            token = data['200']
+
+            # tcp接続を閉じる
+            sock_tcp.close()
+            break
 
 finally:
-    print('closing socket')
-    sock.close()
+    print('closing tcp_socket')
+    sock_tcp.close()
+
+# ここからUDP接続の処理
+
+# AF_INETを使用し、UDPソケットを作成
+sock_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+address = ''
+port = 9051
+
+sock_udp.bind((address, port))
+
+# タイムアウトの設定
+timeout = 100
+last_send_time = time.time()
+
+# 応答受信用スレッドの開始
+thread = threading.Thread(target=recieve_response, args=(sock_udp,))
+thread.daemon = True
+thread.start()
+
+while True:
+    message = input('メッセージを入力してください : ')
+    message_bits = message.encode('utf-8')
+    message_bits_length = len(message_bits)
+
+    # 4094バイト以上のメッセージは送信できないようにする
+    if message_bits_length > 4094:
+        print('メッセージの最大は4094バイトです')
+        continue
+
+    # messageの送信時間
+    current_time = time.time()
+
+    # 一定時間経過したかどうかチェック
+    if current_time - last_send_time >= timeout:
+        print('タイムアウトです')
+        sock_udp.close()
+        break
+
+    try:
+        print('sending {!r}'.format(message))
+        # ヘッダー情報の作成
+        room_name_bits = room_name.encode('utf-8')
+        header = protocol_udp_header(len(room_name_bits), len(token))
+
+        # サーバーへヘッダーの送信
+        sock_udp.sendto(header, (server_address, udp_sever_port))
+
+        # サーバーへルーム名の送信
+        sock_udp.sendto(room_name_bits, (server_address, udp_sever_port))
+
+        # サーバーへトークン文字列の送信
+        sock_udp.sendto(base64.b64decode(token), (server_address, udp_sever_port))
+
+        # サーバーへメッセージの送信
+        sock_udp.sendto(message_bits, (server_address, udp_sever_port))
+
+        # 最後の送信時間の更新
+        last_send_time = current_time
+
+    except Exception as e:
+        print(f"予期しないエラーが発生しました: {e}")
+        print('closing socket')
+        sock_udp.close()
--- app/stage2/server.py
+++ app/stage2/server.py
@@ -1,25 +1,199 @@
 import socket
+import os
+import base64
+import json
+import select
+import time
+import threading
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = '0.0.0.0'
-server_port = 9001
+# トークンリストに該当のIPアドレスとトークンの組み合わせがあるか検索する関数
+def find_token_by_ip(token_list, ip_address):
+    for entry in token_list:
+        if entry['ip'] == ip_address:
+            return True
+    return False
 
-print('Starting up on {} port {}'.format(server_address, server_port))
-
-sock.bind((server_address, server_port))
-
-sock.listen(1)
-
-while True:
-    conneciton, client_address = sock.accept()
+# TCPの処理を行う関数
+def handle_tcp_cilent(connection, client_address, room_info):
     try:
         print('connection from', client_address)
-        header = conneciton.recv(32)
-        print(header)
+
+        while True:
+            header = connection.recv(32)
+            if not header:
+                break
+            room_name_length = int.from_bytes(header[:1], "big")
+            operation = int.from_bytes(header[1:2], "big")
+            state = int.from_bytes(header[3:4], "big")
+            operation_payload_length = int.from_bytes(header[5:32], "big")
+
+            room_name = connection.recv(room_name_length).decode('utf-8')
+            operation_payload = connection.recv(operation_payload_length).decode('utf-8')
+
+            # クライアントから送られてくる操作コードが0のとき
+            if operation == 0:
+                room_info = {}
+                print('サーバ情報を初期化しました')
+
+            # クライアントから送られてくる操作コードが1のとき
+            if operation == 1:
+
+                # 189バイトのランダムバイト列を作成
+                raw_token = os.urandom(189)
+
+                # バイト列をBase64でエンコード
+                encoded_token = base64.b64encode(raw_token).decode('utf-8')
+
+                # ルーム名とユーザー情報を登録
+                # ルーム名が存在しない場合は初期化
+                if room_name not in room_info:
+                    room_info[room_name] = []
+
+                room_info[room_name].append({'ip': client_address[0], 'token': encoded_token})
+
+                # テスト用のユーザー情報
+                room_info[room_name].append({'ip': '0.0.0.0', 'token': 'test'})
+
+                # 生成したトークンとステータスコードをクライアントに送信
+                operation_payload_dict = {'200': encoded_token}
+                print(operation_payload_dict)
+                operation_payload_json = json.dumps(operation_payload_dict)
+                operation_payload_bits = operation_payload_json.encode('utf-8')
+                connection.sendall(operation_payload_bits)
+                print('{}を作成しました'.format(room_name))
+
+            # クライアントから送られてくる操作コードが2のとき
+            if operation == 2:
+
+                # 189バイトのランダムバイト列を作成
+                raw_token = os.urandom(189)
+
+                # バイト列をBase64でエンコード
+                encoded_token = base64.b64encode(raw_token).decode('utf-8')
+
+                # ルーム名とユーザー情報を登録
+                # ルーム名が存在しない場合は初期化
+                if room_name not in room_info:
+                    room_info[room_name] = []
+
+                room_info[room_name].append({'ip': client_address[0], 'token': encoded_token})
+
+                # テスト用のユーザー情報
+                room_info[room_name].append({'ip': '0.0.0.0', 'token': 'test'})
+
+                # 生成したトークンとステータスコードをクライアントに送信
+                operation_payload_dict = {'200': encoded_token}
+                operation_payload_json = json.dumps(operation_payload_dict)
+                operation_payload_bits = operation_payload_json.encode('utf-8')
+                connection.sendall(operation_payload_bits)
 
     except Exception as e:
         print('Error: ' + str(e))
-
     finally:
-        print("Closing current connection")
-        conneciton.close()
+        connection.close()
+        print('operation完了')
+
+# TCPサーバーを開始する関数
+def start_tcp_server(room_info):
+
+    # TCP接続の開始
+    sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    print('Starting up on {} port {}'.format(server_address, tcp_server_port))
+
+    sock_tcp.bind((server_address, tcp_server_port))
+
+    sock_tcp.listen(1)
+
+    while True:
+        connection, client_address = sock_tcp.accept()
+        client_thread = threading.Thread(target=handle_tcp_cilent, args=(connection, client_address, room_info))
+        client_thread.start()
+
+# ルームごとの情報
+room_info = {}
+
+# UDPサーバーを開始する関数
+def start_udp_server(room_info):
+
+    # AF_INETを使用し、UDPソケットを作成
+    sock_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    print('Starting up on {} port {}'.format(server_address, udp_server_port))
+
+    # ソケットをアドレスとポートに紐付け
+    sock_udp.bind((server_address, udp_server_port))
+
+    # タイムアウトの設定
+    timeout = 100
+
+    # 接続しているユーザー情報
+    user_info = {}
+    address = {}
+
+
+    while True:
+        print('\nwaiting to receive message')
+
+        # selectを使って読み取り可能なソケットを監視
+        ready = select.select([sock_udp], [], [])
+
+        if ready[0]:
+            # ヘッダー情報の受信
+            header, address = sock_udp.recvfrom(2)
+            room_name_size = int.from_bytes(header[:1], "big")
+            token_size = int.from_bytes(header[1:2], "big")
+
+            # ルーム名、トークンの受信
+            room_name = sock_udp.recvfrom(room_name_size)[0].decode('utf-8')
+            token = base64.b64encode(sock_udp.recvfrom(token_size)[0]).decode('utf-8')
+
+            # トークンリストとクライアントのトークンの照合
+            if find_token_by_ip(room_info[room_name], address[0]):
+
+                # 現在時刻の取得
+                current_receive_time = time.time()
+
+                # ユーザー情報にipアドレス、ポート番号と最終メッセージ受信時刻を登録
+                user_info[address] = current_receive_time
+
+                # メッセージの受信
+                message = sock_udp.recvfrom(4094)[0]
+
+                if message:
+                    for key in user_info.keys():
+                        sock_udp.sendto(message, key)
+                        print('sending {} {}'.format(message, key))
+
+            else:
+                print('トークンが一致しません')
+        else:
+            if address in user_info:
+
+                # ユーザー情報の削除
+                current_time = time.time()
+                for value in user_info.values():
+                    if current_time - value >= timeout:
+                        print(f'No message received from {address} within the timeout period. Removing client.')
+                        del user_info[address]
+
+                # トークンの削除
+                filtered_data = []
+                for entry in room_info[room_name]:
+                    if entry['ip'] != address[0]:
+                        filtered_data.append(entry)
+                        room_info[room_name] = filtered_data
+
+if __name__ == "__main__":
+    room_info = {}
+    server_address = '0.0.0.0'
+    tcp_server_port = 9001
+    udp_server_port = 9002
+    tcp_thread = threading.Thread(target=start_tcp_server, args=(room_info,))
+    udp_thread = threading.Thread(target=start_udp_server, args=(room_info,))
+
+    tcp_thread.start()
+    udp_thread.start()
+
+    tcp_thread.join()
+    udp_thread.join()
